13e183e63610456f81004065ebab248ab54daefd
Tim Pomaville <tpomavil@gmail.com>
2025-11-12 13:39:18 -0800

    adding streaming demo

--- airflow/dags/dbt_streaming_refresh.py
+++ airflow/dags/dbt_streaming_refresh.py
@@ -1,14 +1,38 @@
-run_dbt = BashOperator(
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from kubernetes.client import models as k8s
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 11, 12),
+    'retries': 1,
+    'retry_delay': timedelta(seconds=30),
+}
+
+dag = DAG(
+    'dbt_streaming_refresh',
+    default_args=default_args,
+    description='Run dbt every 2 minutes via KubernetesPodOperator',
+    schedule_interval='*/2 * * * *',
+    catchup=False,
+    tags=['dbt', 'streaming'],
+)
+
+run_dbt = KubernetesPodOperator(
     task_id='run_dbt_models',
-    bash_command='''
-        cd /opt/airflow/dbt_project && \
-        dbt run
-    ''',
-    env={
-        'POSTGRES_HOST': '172.16.0.20',
-        'POSTGRES_DB': 'myapp',
-        'POSTGRES_USER': 'streamuser',
-        'POSTGRES_PASSWORD': 'w84me@69',  # Or use Airflow connection
-    },
+    name='dbt-streaming-run',
+    namespace='streaming-demo',
+    image='ghcr.io/tpomavil46/k8s-data-platform/dbt-streaming-demo:latest',
+    cmds=['dbt', 'run'],
+    env_from=[
+        k8s.V1EnvFromSource(
+            secret_ref=k8s.V1SecretEnvSource(name='dbt-env')
+        )
+    ],
+    get_logs=True,
+    is_delete_operator_pod=True,
+    in_cluster=True,
     dag=dag,
 )
